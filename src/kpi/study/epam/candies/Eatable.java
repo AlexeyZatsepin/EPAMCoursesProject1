8d671d498389c12d8fecfd2f40a580d236d1da58
Alexey Zatsepin <alexzatsepin7@gmail.com>
2016-06-03 12:56:41 +0300

    jcc and update builder for package

---
+++ src/kpi/study/epam/candies/Eatable.java
@@ -0,0 +1,10 @@
+package kpi.study.epam.candies;
+
+/**
+ * EPAM_Project1_NewYearSurprises
+ * Created 6/2/16, with IntelliJ IDEA
+ *
+ * @author Alex
+ */
+public class Eatable {
+}
---
+++ src/kpi/study/epam/packing/PackageOutOfBoundsExeption.java
@@ -0,0 +1,10 @@
+package kpi.study.epam.packing;
+
+/**
+ * EPAM_Project1_NewYearSurprises
+ * Created 6/3/16, with IntelliJ IDEA
+ *
+ * @author Alex
+ */
+public class PackageOutOfBoundsExeption {
+}
